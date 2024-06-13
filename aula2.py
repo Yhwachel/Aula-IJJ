0804becec7d4424ea0c99a1457e49b7300fad8b4
Yhwachel <yhwachel@gmail.com>
2024-06-13 20:55:15 -0300

    tipos de format

---
+++ aula2.py
@@ -0,0 +1,9 @@
+nome = 'Paula Martins.'
+mes = 'janeiro'
+valor = 'R$500.00'
+desconto = '10%'
+cupom = 'PAULAE10.'
+
+format_1 = f"O nome da pessoa é {nome}, o desconto dela é {desconto}"
+format_2 = "O nome da pessoa é {}, o desconto dela é {}".format(nome, desconto)
+format_3 = "O nome da pessoa é {a}, o desconto dela é {b}".format(a=nome, b=desconto)
