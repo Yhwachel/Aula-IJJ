d3d0dfb3355f324a5f5ee4a4c5f36ca54f3fb9ea
Yhwachel <yhwachel@gmail.com>
2024-06-13 20:36:04 -0300

    First Commit

---
+++ aula1.py
@@ -0,0 +1,7 @@
+nome = 'Paula Martins.'
+mes = 'janeiro'
+valor = 'R$500.00'
+desconto = '10%'
+cupom = 'PAULAE10.'
+
+print ('Ola' , nome , 'Em' , mes , 'voce realizou uma compra no valor de' , valor , 'e ganhou um desconto de' , desconto , 'em sua proxima compra. Use o cupom' , cupom )
