ff03cc6510d0045e7267ed690561e0e6ea29e44b
Yhwachel <yhwachel@gmail.com>
2024-06-13 21:04:24 -0300

    como usar type

--- aula2.py
+++ aula2.py
@@ -1,9 +1,18 @@
 nome = 'Paula Martins.'
-mes = 'janeiro'
-valor = 'R$500.00'
-desconto = '10%'
-cupom = 'PAULAE10.'
+valor = 500.00
+desconto = 10
+cupom = True
 
 format_1 = f"O nome da pessoa é {nome}, o desconto dela é {desconto}"
 format_2 = "O nome da pessoa é {}, o desconto dela é {}".format(nome, desconto)
 format_3 = "O nome da pessoa é {a}, o desconto dela é {b}".format(a=nome, b=desconto)
+
+tipo_nome = type(nome)
+tipo_valor = type(valor)
+tipo_desconto = type(desconto)
+tipo_cupom = type(cupom)
+
+print(tipo_nome)
+print (tipo_valor)
+print(tipo_desconto)
+print (tipo_cupom)
